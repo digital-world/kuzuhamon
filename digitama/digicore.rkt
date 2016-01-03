57f91822c8ede00fd3157d0a72a3502d2430bd2a
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-01-03 14:25:11 +0800

    Update Mission: the meta-project DigiGnome has changed its name to wisemon

--- digitama/digicore.rkt
+++ digitama/digicore.rkt
@@ -1,13 +1,13 @@
 #lang typed/racket
 
 (provide (all-defined-out))
-(provide (all-from-out "../../DigiGnome/digitama/digicore.rkt"))
-(provide (all-from-out "../../DigiGnome/digitama/emoji.rkt"))
+(provide (all-from-out "../../wisemon/digitama/digicore.rkt"))
+(provide (all-from-out "../../wisemon/digitama/emoji.rkt"))
 
-(require "../../DigiGnome/digitama/digicore.rkt")
-(require "../../DigiGnome/digitama/emoji.rkt")
+(require "../../wisemon/digitama/digicore.rkt")
+(require "../../wisemon/digitama/emoji.rkt")
 
-(current-digimon "Kuzuhamon")
+(current-digimon "kuzuhamon")
 
 (define discipline-name : String (path->string (find-system-path 'run-file)))
 (define digimon-island : (Parameterof Path-String) (make-parameter (build-path (digimon-terminus) "island")))
--- tamer/tamer.rkt
+++ tamer/tamer.rkt
@@ -2,9 +2,9 @@
 
 ;;; To force makefile.rkt counting the required file
 @require{../digitama/digicore.rkt}
-@require{../../DigiGnome/digitama/tamer.rkt}
+@require{../../wisemon/digitama/tamer.rkt}
 
 (provide (all-defined-out))
 (provide (all-from-out "../digitama/digicore.rkt"))
-(provide (all-from-out "../../DigiGnome/digitama/tamer.rkt"))
+(provide (all-from-out "../../wisemon/digitama/tamer.rkt"))
 
